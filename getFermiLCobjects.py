517e4ebdfef82393dba7eef3bd4618031e39bfc7
JohnQuinn1 <john.quinn@ucd.ie>
2017-01-01 17:09:05 +0000

    First commit

---
+++ getFermiLCobjects.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+
+############################################################################################
+
+import argparse
+
+desc="""Script to scrape Fermi-LAT LC web site to get list
+of monitored sources and also query NED to see if 
+extragalactic and to get their redshifts.
+If the redshift of an object is -1 then it was not found
+in the NED database (probably galactic) and if the redshift is '0'
+then it was found in NED but the redshift is unknown."""
+
+
+
+parser = argparse.ArgumentParser(description=desc, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('-F', '--File', type=str, default="",  help='save results in comma-separated format to given filename')
+
+parser.add_argument('-D', '--DecInterval',type=float, nargs=2, metavar=("Dec. low", "Dec. high"),
+                    default=(-90,+90), help='Declination interval in degrees.')
+
+parser.add_argument('-R', '--RAInterval',type=float, nargs=2, metavar=("RA. low", "RA. high"),
+                    default=(0,360), help='RA interval in degrees.')
+
+parser.add_argument('-z','--zInterval',type=float, nargs=2, metavar=("z low", "z high"),
+                    default=(-1000,1000), help='redshift interval.')
+
+parser.add_argument('-v','--verbose', action='store_true', help='print out to screen')
+
+cfg = parser.parse_args()
+
+
+############################################################################################
+
+############################################################################################
+
+def filter(RA, Dec, z, cfg):
+    if (cfg.RAInterval[0] <= RA <= cfg.RAInterval[1]):
+        if(cfg.DecInterval[0] <= Dec <= cfg.DecInterval[1]):
+            if(cfg.zInterval[0] <= z <= cfg.zInterval[1]):
+                return True
+
+    return False
+
+############################################################################################
+
+
+from astroquery.ned import Ned
+from astroquery.exceptions import RemoteServiceError
+
+
+import requests
+URL='http://fermi.gsfc.nasa.gov/ssc/data/access/lat/msl_lc/'
+page=requests.get(URL)
+if cfg.verbose: print("Successfully downloaded:",URL)
+
+
+from bs4 import BeautifulSoup
+soup=BeautifulSoup(page.content,'lxml')
+
+table=soup.find("table")
+
+names=[]
+ras=[]
+decs=[]
+zs=[]
+
+nobjects=0
+naccepted=0
+
+for row in table.findAll("tr"):
+    cells=row.findAll("td")
+    # Every first row has 3 cells while every second row only has two cells
+    # The first cell of the first row (i.e cell[0]) contains the source details at 
+    # the start. 
+
+    if(len(cells)==3):
+        nobjects+=1
+
+        t=cells[0].getText()
+
+        # The format is along the lines of:
+        # '\n1ES 2344+514(RA = 356.770, Dec = 51.7050)\n\t\t\nDaily Light Curve\nDaily Light Curve (1yr)\nDaily Light Curve Fits File\nWeekly Light Curve\nWeekly Light Curve (1yr)\nWeekly Light Curve Fits File\n\n'
+
+        name=t[1:t.find("(")]
+        
+        ra=float(t[t.find('(RA')+5:t.find(',')])
+        dec=float(t[t.find(', Dec')+7:t.find(')')])
+
+        if cfg.verbose: print("Found LAT LC for:",name,"now quering NED...", end="")
+
+
+        # Now query NED...
+
+        try:
+            q=Ned.query_object(name)
+            nedz=q['Redshift']
+            if nedz.mask: # True if invalid/data missing (i.e. redshift missing)
+                z=0.0
+                if cfg.verbose: print("found! no z!, assigning 0.0")
+            else:
+                z=float(nedz)
+                if cfg.verbose: print("found! z=",z)
+        except RemoteServiceError: # i.e. object not found
+            if cfg.verbose: print("not found! assigning z=-1")
+            z=-1
+            
+        if filter(ra,dec,z,cfg):
+#            if cfg.verbose: print(name,"accepted by filter")
+            names.append(name)
+            ras.append(ra)
+            decs.append(dec)
+            zs.append(z)
+            naccepted+=1
+        else:
+ #           if cfg.verbose:print(name,"rejected by filter")
+            pass
+
+
+
+# find longest name length for printing
+maxw=max([len(name) for name in names])
+
+
+if cfg.verbose:
+    print()
+    print("Accepted",naccepted,"of",nobjects,"objects:")
+    for name, ra, dec, z in zip(names, ras, decs, zs):
+        print("{name:{maxw}s} {ra:8.3f}  {dec:7.3f}  {z:7.4f}".format(name=name, maxw=maxw,ra=ra, dec=dec, z=z))
+
+if cfg.File:
+    if cfg.verbose: print("Saving to",cfg.File)
+    with open(cfg.File,"w") as f:
+        for name, ra, dec, z in zip(names, ras, decs, zs):
+            f.write("{}, {},  {},  {}, \n".format(name, ra, dec, z))
+        
+
+    
+
---
+++ plotFermiLC.py
@@ -0,0 +1,186 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+from __future__ import division
+
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import numpy as np
+
+import sys
+#if len(sys.argv) = 2:
+#    print("Usage: {} 'object name'".format(sys.argv[0]))
+#    sys.exit(0)
+          
+
+import argparse
+desc="""Download and plot Fermi-LAT light curves. 
+Only data points are plotted - upper limits are ignored.
+Note: the Fermi lightcurve names have all spaces removed and 
+are in upper case - thus giving names like "cta 102" will
+automatically be translated to "CTA102". Also, the y-axis scaling
+is set by the range of fluxes in the entire file.""" 
+
+parser = argparse.ArgumentParser(description=desc, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('-d','--days', type=int, default=100, help="recent number of days to plot")
+
+parser.add_argument('-r','--remove_FITS', action='store_true', help='remove FITS file after processing')
+
+parser.add_argument('-a','--already_downloaded', action='store_true', help="use already-downloaded FITS file - don't redownload")
+
+parser.add_argument('-w','--weekly', action='store_true', help='download and plot weekly light curve instead of daily')
+
+parser.add_argument('-c','--column_info', action='store_true', help='print column info from FITS file')
+
+parser.add_argument('-s','--stdout', action='store_true', help='print data to std out')
+
+parser.add_argument('-p','--plot_window', action='store_true', help='open plot window (otherwise plot produced just on disk')
+
+parser.add_argument('-n','--no_png', action='store_true', help="don't save png file")
+
+parser.add_argument('-e','--energy_range', type=str, choices=['FLUX_1000_300000', 'FLUX_300_1000', 'FLUX_100_300000'],
+                    default='FLUX_100_300000', help='choose energy range')
+
+parser.add_argument('name',type=str, help='name of object to be downloaded')
+
+
+parser.add_argument('-v', '--verbose', action='store_true', help='show what is going on')
+
+cfg = parser.parse_args()
+
+
+
+
+# remove spaces in object names to match LAT LC filenames.
+object=cfg.name.replace(" ","")
+FITS=object.upper()
+if cfg.weekly: 
+    FITS+='_604800.lc'
+else:
+    FITS+='_86400.lc'
+
+
+##############################################################
+# download or use already-existing file...
+
+import os
+
+if cfg.already_downloaded:
+    if cfg.verbose: print("Using downloaded",FITS)
+    
+else:
+    import wget
+
+    if cfg.verbose:
+        bar_style=wget.bar_adaptive
+    else:
+        bar_style=None
+
+
+    # Daily
+    if os.path.isfile(FITS):
+        if cfg.verbose: print("Removing old file:",FITS)
+        os.remove(FITS)
+
+    if cfg.verbose: print("Downloading latest daily file:",FITS)
+
+    remote_file='http://fermi.gsfc.nasa.gov/FTP/glast/data/lat/catalogs/asp/current/lightcurves/'+FITS
+
+    if cfg.verbose: print("wget:",remote_file)
+
+    try:
+        from urllib.error import HTTPError # just so I can catch and handle this exception
+        x=wget.download(remote_file, bar=bar_style)
+    except HTTPError as e:
+        print(e)
+        print("Exiting...")
+        sys.exit(1)
+    except:
+        print("Some error occurred...")
+        raise
+        
+
+    if cfg.verbose: print()
+
+
+##############################################################
+
+try:
+    hdulist = fits.open(FITS)
+except:
+    print("Error opening file:",FITS)
+    print("Exiting...")
+    sys.exit(1)
+
+tbdata=hdulist[1].data
+
+if cfg.column_info:
+    cols = hdulist[1].columns
+    print(cols.info())
+
+F=cfg.energy_range
+if F=='FLUX_100_300000':
+    FERR='ERROR_100_300000'
+    UL='UL_100_300000'
+elif F=='FLUX_300_1000':
+    FERR='ERROR_300_1000'
+    UL='UL_300_1000'
+elif F=='FLUX_1000_300000':
+    FERR='ERROR_1000_300000'
+    UL='UL_1000_300000'
+
+
+f=np.array(tbdata[F])
+fe=np.array(tbdata[FERR])
+x=(np.array(tbdata['START']) + np.array(tbdata['STOP']))/2
+dx=(x-np.array(tbdata['START']))/(60*60*24) 
+TS=np.array(tbdata['TEST_STATISTIC'])
+ulf=np.array(tbdata[UL])
+
+t=51910+x/(60*60*24)  # convert mission secs to MJD                                                                    
+
+# which points are ULs?
+i=np.where(ulf==False)
+
+
+if cfg.stdout:
+    if cfg.verbose:
+        print("Printing data to stdout:")
+        print("{:8s}  {:4s}  {:8s}  {:8s}".format("MJD_mid", "dMJD", "Flux", "Flux_err"))
+    for j in i[0]: # where returns a tuple of arrays so the first element is the array!
+        print("{:8.2f}  {:4.2f}  {:3.2e}  {:3.2e}".format(t[j], dx[j], f[j], fe[j]))
+
+
+plt.clf()
+plt.errorbar(t[i],f[i],fe[i],xerr=dx[i],fmt='o')
+timescale= "weekly" if cfg.weekly else "daily"
+plt.title('{} Fermi-LAT {} light curve'.format(object,timescale))
+plt.xlabel('MJD')
+plt.ylabel('{} '.format(F)+'($ph\,cm^{-2}\,s^{-1}$)')
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+ax=plt.gca()
+ax.yaxis.major.formatter._useMathText=True
+tmax=int(max(t)+dx[-1]+1.5)
+tmin=int(max(t)-cfg.days)
+plt.axis(xmin=tmin)
+plt.axis(xmax=tmax)
+#plt.axis(ymax=0.8e-5)
+plt.grid()
+
+
+if not cfg.no_png:
+    pngfile=object+"_"+timescale+"_"+F+".png"
+    if cfg.verbose: print("Saving",pngfile)
+    plt.savefig(pngfile)
+
+
+if cfg.plot_window:
+    print("Please manualy close figure window for program to continue!")
+    plt.show()
+
+
+if cfg.remove_FITS:
+    if cfg.verbose: print("Removing",FITS)
+    os.remove(FITS)
+
